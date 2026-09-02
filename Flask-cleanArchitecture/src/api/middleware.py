046c723d18b83696adf798b2f9fff13817dad088
Thanh Nhan <nahnht203@gmail.com>
2026-09-02 15:15:17 +0700

    feat: implement authentication frontend and backend

--- Flask-cleanArchitecture/src/api/middleware.py
+++ Flask-cleanArchitecture/src/api/middleware.py
@@ -1,27 +1,50 @@
-# Middleware functions for processing requests and responses
+from flask import request, jsonify
 
-from flask import  request, jsonify
+
+ALLOWED_ORIGINS = {
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+}
+
 
 def log_request_info(app):
-    app.logger.debug('Headers: %s', request.headers)
-    app.logger.debug('Body: %s', request.get_data())
+    app.logger.debug("Headers: %s", request.headers)
+    app.logger.debug("Body: %s", request.get_data())
 
-def handle_options_request():
-    return jsonify({'message': 'CORS preflight response'}), 200
 
 def error_handling_middleware(error):
-    response = jsonify({'error': str(error)})
+    response = jsonify({"error": str(error)})
     response.status_code = 500
     return response
 
+
 def add_custom_headers(response):
-    response.headers['X-Custom-Header'] = 'Value'
+    origin = request.headers.get("Origin")
+
+    if origin in ALLOWED_ORIGINS:
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Methods"] = (
+            "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+        )
+        response.headers["Access-Control-Allow-Headers"] = (
+            "Content-Type, Accept, Authorization"
+        )
+        response.headers["Access-Control-Max-Age"] = "3600"
+
+    response.headers["X-Custom-Header"] = "Value"
+
     return response
 
+
 def middleware(app):
+
     @app.before_request
     def before_request():
         log_request_info(app)
+
+        # Handle CORS preflight request
+        if request.method == "OPTIONS":
+            return "", 204
 
     @app.after_request
     def after_request(response):
@@ -30,7 +53,3 @@ def middleware(app):
     @app.errorhandler(Exception)
     def handle_exception(error):
         return error_handling_middleware(error)
-
-    @app.route('/options', methods=['OPTIONS'])
-    def options_route():
-        return handle_options_request()
